a7e78e9a621d76570e5a2f06966dbfda46bad087
TranHuuLoc129 <loctran7129@gmail.com>
2023-10-26 11:45:22 +0700

    #####

---
+++ DAL/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class Connection
+    {
+        private String ConnectionString = "ABC";
+        private SqlConnection conn = null;
+
+
+        public Connection() { }
+
+        public SqlConnection getConnection()
+        {
+
+            conn = new SqlConnection(ConnectionString);
+
+            return conn;
+        }
+
+        public void close()
+        {
+            conn.Close();
+        }
+    }
+}
